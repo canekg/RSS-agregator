ae6b826c43667fcfec8424264f7de7f408e0a7c6
Alexandr <canekg@mail.ru>
2023-09-19 22:56:45 +0500

    added function for implementing events, added state and i18Instance initiation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
 import './styles.scss';
 import 'bootstrap';
+import i18next from 'i18next';
+import app from './app.js';
+import resources from './locales/index.js';
+
+const func = () => {
+  const promise = new Promise((resolve) => {
+    const i18Instance = i18next.createInstance();
+    i18Instance.init({
+      lng: 'ru',
+      debug: true,
+      resources,
+    });
+    resolve(i18Instance);
+  });
+
+  promise
+    .then((i18nInst) => {
+      const state = {
+        i18n: i18nInst,
+        errorMessage: null,
+        currentProcess: 'filling', // fillingRssUrl, loadingRssContent
+        validationUrl: {
+          isValid: null,
+        },
+      };
+      return state;
+    })
+    .then((state) => {
+      app(state);
+    })
+    .catch((e) => {
+      console.log(e, 'error in init');
+    });
+};
+
+func();
