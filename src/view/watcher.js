dc40b9ad2e67b3eb293787fea0f336bbab4f9ee3
Alexandr <canekg@mail.ru>
2023-10-02 01:21:58 +0500

    deleted src/validation/validate.js, src/handlers/handler.js, src/view/watcher.js

--- src/view/watcher.js
+++
@@ -1,8 +0,0 @@
-import onChange from 'on-change';
-import render from '../render/render.js';
-
-const watcher = (state) => onChange(state, (path, value) => {
-  render(state, value, path);
-});
-
-export default watcher;
