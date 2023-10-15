3300f4f2eafb32cbfbaa192b996e791efcdf9d6d
Alexandr <canekg@mail.ru>
2023-10-15 17:24:25 +0500

    modified:   src/locales/ru.js and unnecessary actions removed from the parser

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -3,21 +3,27 @@ export default {
     languages: {
       ru: 'Русский',
     },
-    validation: {
-      errors: {
-        errorURL: 'Ссылка должна быть валидным URL',
-        errorUniqUrl: 'RSS уже существует',
-        errorRequared: 'Не должно быть пустым',
-      },
+    errors: {
+      errorURL: 'Ссылка должна быть валидным URL',
+      errorUniqUrl: 'RSS уже существует',
+      errorRequared: 'Не должно быть пустым',
+      errorNetWork: 'Ошибка сети',
+      errorResource: 'Ресурс не содержит валидный RSS',
     },
-    loading: {
-      errrors: {
-        errorNetWork: 'Ошибка сети',
-        errorResource: 'Ресурс не содержит валидный RSS',
-      },
-      loadingRssContent: 'RSS загружается',
-      loadedRssContent: 'RSS успешно загружен',
-    },
-    // END
+    loadingRssContent: 'RSS загружается',
+    loadedRssContent: 'RSS успешно загружен',
+    textFeeds: 'Фиды',
+    textPosts: 'Посты',
+    textView: 'Просмотр',
+    textMainTitle: 'RSS агрегатор',
+    textLead: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    textForUrlInput: 'RSS ссылка',
+    textButton: 'Добавить',
+    textInfo: 'Пример: https://ru.hexlet.io/lessons.rss',
+    textCreatedBy: 'created by',
+    author: 'Alexandr Fadeev',
+    linkGithubAuthor: 'https://github.com/canekg',
+    textBtnFullArticle: 'Читать полностью',
+    textCloseModalContent: 'Закрыть',
   },
 };
--- src/parsers/parserRss.js
+++ src/parsers/parserRss.js
@@ -1,13 +1,12 @@
 import _ from 'lodash';
 
-const parserRss = (response, feedId) => {
+const parserRss = (response) => {
   try {
     const parser = new DOMParser();
     const data = parser.parseFromString(response.data.contents, 'application/xml');
     const feed = {
       title: data.querySelector('channel title').textContent,
       description: data.querySelector('channel description').textContent,
-      feedId,
     };
 
     const items = Array.from(data.querySelectorAll('item'));
@@ -15,14 +14,13 @@ const parserRss = (response, feedId) => {
       const title = item.querySelector('title').textContent;
       const description = item.querySelector('description').textContent;
       const link = item.querySelector('link').textContent;
-      const id = _.uniqueId();
       return {
-        title, description, link, feedId, id,
+        title, description, link,
       };
     });
     return { feed, posts };
   } catch {
-    throw new Error('errorParsing');
+    throw new Error('errorResource');
   }
 };
 
